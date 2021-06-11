c98840e3925fdf5587a6a883ffa3ab1be14d2e9e
ddas09 <dipankar62918@gmail.com>
2021-06-11 14:40:31 +0530

    implemented PUT and POST method

--- http_socket.py
+++ http_socket.py
@@ -5,25 +5,45 @@ import sys
 def sendHttpRequest(choice, socket):
     if choice == '1':
         req = "GET / HTTP/1.1\r\nHost:localhost\r\nConnection: close\r\n\r\n"
+
     elif choice == '2':
         req = "GET /api/stocks/ HTTP/1.1\r\nHost:localhost\r\nConnection: close\r\n\r\n"
+
     elif choice == '3':
         id = input("Enter stock id: ")
         req = f"GET /api/stocks/{id}/ HTTP/1.1\r\nHost:localhost\r\nConnection: close\r\n\r\n"
+
     elif choice == '4':
-        pass
+        symbol = input("Enter stock symbol: ")
+        name = input("Enter stock name: ")
+        req_body = f'{{"symbol": "{symbol}", "name": "{name}"}}'
+        data_length = len(req_body)
+        req = "POST /api/stocks/ HTTP/1.1\r\nHost:localhost\r\nContent-Type: application/json"
+        req += f"\r\nContent-Length:{data_length}\r\nConnection: close\r\n\r\n{req_body}"
+
     elif choice == '5':
-        pass
+        id = input("Enter the stock id to update: ")
+        symbol = input("Enter new symbol: ")
+        name = input("Enter new name: ")
+        req_body = f'{{"symbol": "{symbol}", "name": "{name}"}}'
+        data_length = len(req_body)
+        req = f"PUT /api/stocks/{id}/ HTTP/1.1\r\nHost:localhost\r\nContent-Type: application/json"
+        req += f"\r\nContent-Length:{data_length}\r\nConnection: close\r\n\r\n{req_body}"
+
     elif choice == '6':
-        pass
+        id = input("Enter the stock id to delete: ")
+        req = f"DELETE /api/stocks/{id}/ HTTP/1.1\r\nHost:localhost\r\nConnection: close\r\n\r\n"
+        
     else:
         return "Invalid choice"
 
     # Send request to server
     socket.sendall(req.encode())
     response = ''
+
+    # Recieve all data packets
     while True:
-        server_response = client_socket.recv(4096)
+        server_response = client_socket.recv(4096)  
         if not server_response:
             break
         else:
@@ -37,7 +57,7 @@ if __name__ == '__main__':
     server_port = 8000
     
     while True:
-        print("\n1. Index Page\n2. Get all stocks\n3. Get stock by id", end="")
+        print("\n1. Index page\n2. Get all stocks\n3. Get stock by id", end="")
         print("\n4. Insert new stock\n5. Update a stock\n6. Delete a stock\n7. Quit\n")
         choice = input("Enter your choice: ")
         if choice == '7':
